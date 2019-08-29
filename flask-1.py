48fe9f4f34a47e03b99aba6bc25a642c28557b2d
Bjarkikop <bjarki00@simnet.is>
2019-08-29 14:21:34 +0000

    flask

---
+++ flask-1.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return '<h1>Flask er flottast</h1>'
+@app.route('/biography')
+def Biography():
+	return '<h1>Þetta er Biography síða</h1>'
+
+@app.route('/about')
+def about():
+	return '<h1>Þetta er about síðan </h1>'
+@app.route("/home", methods=['GET', 'POST'])
+def home():
+	links = ['https://www.youtube.com', 'https://www.bing.com', 'https://www.python.org']
+	return render_template('example.html', myvar="flask example", links=links)
+
+if __name__ == "__main__":
+	app.run(debug=True)
+	
