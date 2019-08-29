c7e989e88aeaa5a773908c43121ec9b70d1f7904
Bjarkikop <bjarki00@simnet.is>
2019-08-29 15:05:14 +0000

    req og proc

--- flask-1.py
+++ flask-1.py
@@ -6,11 +6,11 @@ def index():
 	return '<h1>Flask er flottast</h1>'
 @app.route('/biography')
 def Biography():
-	return '<h1>Þetta er Biography síða</h1>'
+	return '<h1>Þetta er Biography síða</h1>  <a href="/home">Home</a>'
 
 @app.route('/about')
 def about():
-	return '<h1>Þetta er about síðan </h1>'
+	return '<h1>Þetta er about síðan </h1>  <a href="/home">Home</a>'
 @app.route("/home", methods=['GET', 'POST'])
 def home():
 	links = ['https://www.youtube.com', 'https://www.bing.com', 'https://www.python.org']
